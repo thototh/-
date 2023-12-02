bef9e2e5b8b1c3a45bf4a0eabc79103b11e60550
thototh <144813512+thototh@users.noreply.github.com>
2023-12-02 17:16:42 +0900

    게임 재시작 기능 추가

--- src/Omok.java
+++ src/Omok.java
@@ -52,11 +52,35 @@ class Omok extends JFrame {
             if (judge.checkWin(board)) {
                 if (board.state == 'B') {  // if black wins
                     JOptionPane.showMessageDialog(null, "BLACK WINS!", "GAME OVER", JOptionPane.INFORMATION_MESSAGE);
+					
                 } else {    // if white wins
                     JOptionPane.showMessageDialog(null, "WHITE WINS!", "GAME OVER", JOptionPane.INFORMATION_MESSAGE);
                 }
+				resetGame();   //게임 종료 후 ok 버튼을 클릭하면 게임을 다시 시작할 수 있도록 함
             }
-			board.removeActionListener(this);
+		}
+
+		private void resetGame() {   //게임이 끝난뒤 재실행할 수 있도록 하는 메서드
+			for (int i = 0; i < SIZE; i++) {
+				for (int j = 0; j < SIZE; j++) {
+					board[i][j].removeActionListener(this);  //각 좌표의 액션리스너를 제거함
+				}
+			}
+
+			for (int i = 0; i < SIZE; i++) {
+				for (int j = 0; j < SIZE; j++) {
+					board[i][j].setIcon(img);  //돌이 하나도 없는 새로운 판을 만들기 위함
+					board[i][j].state = 'N';
+				}
+			}
+
+			turn = black;  // 다음게임이 흑돌부터 시작하도록 함
+
+			for (int i = 0; i < SIZE; i++) {
+				for (int j = 0; j < SIZE; j++) {
+					board[i][j].addActionListener(this);   //각 좌표에 앞서 제거했던 액션리스너를 다시 추가
+				}
+			}
 		}
 	}
 }
